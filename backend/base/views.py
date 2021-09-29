36a8f22a5accf54c9368fafbcd9f76b1c4ee91f2
monsur22 <monsurahmedshafiq@gmail.com>
2021-09-29 21:56:48 +0600

    protect route

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from .products import products
@@ -32,9 +34,16 @@ def getRoutes(request):
     return Response('Hello')
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user,many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users,many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
